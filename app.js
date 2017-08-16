0c619178cee5a3ca69f3da8550706d46070d1a2a
kylearora <kylearora@gmail.com>
2017-08-16 17:27:23 -0500

    Pushed completed tasks to completed and completed some styling

--- app.js
+++ app.js
@@ -9,9 +9,34 @@ app.use(express.static("public"))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(expressValidator())
 
-app.get("/", function(req, res) {
-  res.render("index", {
-  })
+
+const todos = [
+    "Wash the car",
+    "Learn Node",
+    "Catch up on some projects",
+    "Stop bugging Reggie",
+    "Dont catch the zZz in class"
+];
+
+const completed = [
+  
+];
+
+app.get("/", function (req, res) {
+  res.render('index', {
+    todos: todos,
+    completed: completed
+   });
+});
+
+app.post("/", function (req, res) {
+  todos.push(req.body.todo);
+  res.redirect('/');
+})
+
+app.post("/completed", function (req, res) {
+  completed.push(req.body.button);
+  res.redirect('/');
 })
 
 app.listen(3000, function() {
